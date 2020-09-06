71a8858176bae2163aaeb66ae5ab8b5c427371f8
SimonaBarale <barale.simona13@gmail.com>
2020-09-06 19:32:37 +0200

    third commit

--- alterna.js
+++ alterna.js
@@ -1,41 +1,13 @@
-var loaded = false;
-var i = 0;
-
-var img1;
-var img2;
-
 function changeBackground(img1, img2) {
-
-    this.img1 = img1;
-    this.img2 = img2;
-
-    /*var classe1 = document.createElement('style');
-    classe1.innerHTML =
-        ".frame.img1 {" +
-        "background:url('" + img1 + "') no-repeat center / auto 100%;" +
-        "}";
-
-    var classe2 = document.createElement('style');
-    classe2.innerHTML =
-        ".frame.img2 {" +
-        "background:url('" + img2 + "') no-repeat center / auto 100%;" +
-        "}";
-
-    var ref = document.querySelector('script');
-
-    ref.parentNode.insertBefore(classe1, ref);
-    ref.parentNode.insertBefore(classe2, ref);
-*/
 
     var div = document.querySelector('.frame-mobile');
     div.style.background = "url('" + img1 + "') no-repeat center / auto 100%";
 
-    i = 1;
+    var i = 1;
     setInterval(displayNextImage, 2000);
 
     div = document.querySelector('.frame');
     div.style.background = "url('" + img1 + "') no-repeat center / auto 100%";
-    //images.classList.toggle('img1');
 
     function displayNextImage() {
         var div = document.querySelector('.frame');
@@ -45,5 +17,6 @@ function changeBackground(img1, img2) {
             div.style.background = "url('" + img1 + "') no-repeat center / auto 100%";
         i = i == 1 ? 0 : 1;
     }
+
 }
 
--- nav.js
+++ nav.js
@@ -1,3 +1,9 @@
+
+
+/*  Cerca nel documento un tag html che abbia id = #resize 
+    Applica oppure toglie la classe .active a tale elemento    
+*/
+
 function toggleMenu() {
     var menu = document.querySelector('#resize');
     menu.classList.toggle('active');
